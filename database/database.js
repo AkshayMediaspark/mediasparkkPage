c39afd277185067220e7609249c860239532f43c
mediasparkk <akshaymediaspark@gmail.com>
2023-10-19 11:32:04 +0530

    database modifi

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 
 
-const passward = process.env.mongodb_passward
+const url = process.env.URL
 
 
 
@@ -10,7 +10,7 @@ const passward = process.env.mongodb_passward
 
 
 const mongoconnect = async ()=>{
-    await mongoose.connect(`mongodb+srv://akshaymediaspark:${passward}@cluster0.jsxnsvm.mongodb.net/?retryWrites=true&w=majority`)
+    await mongoose.connect(url)
 
     console.log(" data base connected")
 }
