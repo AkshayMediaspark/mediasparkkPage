f7618270bc52c947b32aa0bc94033fc6857ca3d8
mediasparkk <akshaymediaspark@gmail.com>
2023-10-19 10:42:07 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
-const port = process.env.PORT
+const port = process.env.PORT || 8080
 
 
 
